e9ccc3ddf052360df023d1977fafb45fb0cc73c0
Dan Rigsby <danrigsby@gmail.com>
2015-11-29 09:34:04 -0500

    props not available

--- IndexAPIAppLinkResolver.js
+++ IndexAPIAppLinkResolver.js
@@ -16,7 +16,7 @@ var AppLink = require('./AppLink');
 
 class IndexAPIAppLinkResolver extends AppLinkResolver {
   constructor(facebook_token: String) {
-    super(this.props);
+    super();
     this._facebookToken = facebook_token;
   }
 
